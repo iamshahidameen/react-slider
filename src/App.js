2acadc44375287fb470f30e2ecd6c3407d93f7c3
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-07-22 22:47:37 +0500

    Added sliding functionality, with next and prev buttons

--- src/App.js
+++ src/App.js
@@ -3,40 +3,87 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
-  return (
-    <>
-      <section className="section">
-        <div className="title">
-          <h2>
-            <span>/</span>reviews
-          </h2>
-        </div>
-        <div className="section-center">
-          <article className="nextSlide">
-            <img
-              src="https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg"
-              alt="maria ferguson"
-              className="person-img"
-            />
-            <h4>maria ferguson</h4>
-            <p className="title">office manager</p>
-            <p className="text">
-              Fingerstache umami squid, kinfolk subway tile selvage tumblr man
-              braid viral kombucha gentrify fanny pack raclette pok pok
-              mustache.
-            </p>
-            <FaQuoteRight className="icon" />
-          </article>
-          <button className="prev">
-            <FiChevronLeft />
-          </button>
-          <button className="next">
-            <FiChevronRight />
-          </button>
-        </div>
-      </section>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const [reviews, setReviews] = useState([]);
+  const [value, setValue] = useState(0);
+  function getReviews() {
+    setLoading(false);
+    setReviews(data);
+  }
+
+  useEffect(() => {
+    getReviews();
+  });
+  console.log(reviews);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  } else {
+    const { id, image, name, quote, title } = reviews[value];
+    function slideControl(slideAction) {
+      if (slideAction === 'next') {
+        console.log(value, reviews.length, 'check this');
+
+        if (value >= reviews.length - 1) {
+          setValue(0);
+        } else {
+          setValue(value + 1);
+        }
+      } else {
+        if (value <= 0) {
+          setValue(reviews.length - 1);
+        } else {
+          setValue(value - 1);
+        }
+      }
+    }
+    return (
+      <>
+        <section className="section">
+          <div className="title">
+            <h2>
+              <span>/</span>reviews
+            </h2>
+          </div>
+          <div className="section-center">
+            {/* {reviews.map((review) => {
+              console.log(review);
+              const { id, image, name, quote, title } = review;
+              return (
+                <article className="nextSlide" key={id}>
+                  <img
+                    src={image}
+                    alt="maria ferguson"
+                    className="person-img"
+                  />
+                  <h4>{name}</h4>
+                  <p className="title">{title}</p>
+                  <p className="text">{quote}</p>
+                  <FaQuoteRight className="icon" />
+                </article>
+              );
+            })} */}
+
+            {
+              <article className="nextSlide" key={id}>
+                <img src={image} alt="maria ferguson" className="person-img" />
+                <h4>{name}</h4>
+                <p className="title">{title}</p>
+                <p className="text">{quote}</p>
+                <FaQuoteRight className="icon" />
+              </article>
+            }
+            <button className="prev" onClick={() => slideControl('prev')}>
+              <FiChevronLeft />
+            </button>
+            <button className="next" onClick={() => slideControl('next')}>
+              <FiChevronRight />
+            </button>
+          </div>
+        </section>
+      </>
+    );
+  }
 }
 
 export default App;
